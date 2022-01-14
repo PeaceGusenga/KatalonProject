4601d0d7421a119c73c03c3be4e5cf556bd58381
PEACE <PEACE@10.0.0.111>
2022-01-14 14:00:16 +0200

    > TestSuite 1

---
+++ Test Suites/TestSuite 1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>320c2562-5a15-414e-852f-5d2a83e3a867</testSuiteGuid>
+</TestSuiteEntity>
